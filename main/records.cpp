409c9b5f3c279344ddc8201002bb161f63d74114
Daniel Melancon <dmelancon@gmail.com>
2024-12-02 22:11:39 -0800

    Starting to implement the classes for records.

--- main/records.cpp
+++ main/records.cpp
@@ -1,4 +1,8 @@
+#include "records.h"
 // records.cpp
 // CS 314 Group 7 
 // Fall 2024
 
+Provider::Provider(string a_name, Address a_addr)
+{
+}
--- main/records.h
+++ main/records.h
@@ -8,6 +8,8 @@ using namespace std;
 #include <string.h>
 #include <iostream>
 
+#include "address.h"
+
 // Address string lengths
 const int ADDR_HOUSE_LEN = 25;
 const int ADDR_CITY_LEN = 14;
@@ -17,3 +19,47 @@ const int ADDR_ZIP_LEN = 5;
 // generic user constants
 const int USR_ID_MAX = 999999999; // restrain ID values to 9 digits
 const int USR_NAME_LEN = 25;
+
+class User
+{
+public:
+    User(string a_name, Address a_addr);
+
+    int id;
+    string name;
+    Address address;
+
+    void display();
+
+};
+
+class Member : public User
+{
+public:
+    Member(string a_name, Address a_addr, bool status)
+    : User(a_name, a_addr) {}
+
+    bool status;
+};
+
+class Provider : public User
+{
+public:
+    Provider( string a_name, Address a_addr)
+    : User(a_name, a_addr) {}
+};
+
+class Service
+{
+public:
+    Service(int a_id, string a_name);
+
+    int id;
+    string name;
+};
+
+User::User(string a_name, Address a_addr)
+{
+    name = a_name;
+    address = a_addr;
+}
