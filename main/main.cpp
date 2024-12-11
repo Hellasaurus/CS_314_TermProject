c68172aa01648dc247423884f22d1df6d528ee86
Daniel Melancon <dmelancon@gmail.com>
2024-12-10 22:15:41 -0800

    Starting the CLI

--- main/main.cpp
+++ main/main.cpp
@@ -17,10 +17,39 @@ const string memPath = "./users/users_10.csv";
 const string proPath = "./users/provider_list.csv";
 const string svcPath = "./users/service_list.csv";
 
-int main(int argc, char **argv) {
-    {
-        for (int i = 0; i < argc; i++) {
-                }
+const size_t MAX_ARG_LEN = 100;
+
+const int MODE_INVALID = -1;
+const int MODE_HELP = 0;
+const int MODE_PRO = 1;
+const int MODE_ACC = 2;
+const int MODE_MGMT = 3;
+
+const string helpstring =
+    "ChocAnon Data Processing Software Module Demo!\n\
+CS 314 Group #7\n\n\
+This program can be run in the following demo modes:\n\
+\t-h : help\n\
+\t-p : provider mode\n\
+\t-a : accounting mode\n\
+\t-m : manager mode\n\
+\t-v : verbose\n\n";
+
+int parseopt(int argc, char** argv, int& verbose);
+
+int main(int argc, char** argv) {
+    // bail if no argument
+    if (argc <= 1) {
+        cout << helpstring;
+        return 0;
+    }
+
+    int verbose = 0;
+    int mode = parseopt(argc, argv, verbose);
+
+    if (mode < 1) {
+        cout << helpstring;
+        return 0;
     }
 
     // initialize manager
@@ -42,4 +71,58 @@ int main(int argc, char **argv) {
     // newAddress.display();
 
     return 0;
+}
+
+int parseopt(int argc, char** argv, int& verbose) {
+    char arg[MAX_ARG_LEN];
+
+    int mode = MODE_INVALID;
+
+    {
+        for (int i = 0; i < argc; i++) {
+            strncpy(arg, argv[i], MAX_ARG_LEN);
+            if (arg[0] == '-') {
+                switch (arg[1]) {
+                    case 'h':  // help mode
+                    case 'H':
+                        return MODE_HELP;
+                        break;
+
+                    case 'v':  // verbosity
+                    case 'V':
+                        cout << "Verbosity has been increased!\n";
+                        verbose++;
+                        break;
+
+                    case 'm':  // manager mode
+                    case 'M':
+                        if (mode == MODE_INVALID) {
+                            mode = MODE_MGMT;
+                            cout << "Starting program in manager mode...\n";
+                        }
+                        break;
+                    case 'p':  // provider mode
+                    case 'P':
+                        if (mode == MODE_INVALID) {
+                            mode = MODE_PRO;
+                            cout << "Starting Program in provider mode...\n";
+                        }
+                        break;
+
+                    case 'a':  // accounting mode
+                    case 'A':
+                        if (mode == MODE_INVALID) {
+                            mode = MODE_ACC;
+                            cout << "Starting Program in Accounting mode...\n";
+                        }
+                        break;
+
+                    default:
+                        mode = MODE_HELP;
+                        break;
+                }
+            }
+        }
+    }
+    return mode;
 }
